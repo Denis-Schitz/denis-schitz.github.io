b58ce5ec14bc026a28d6eafa8205776815ec2ea4
Denis-Schitz <98731402+Denis-Schitz@users.noreply.github.com>
2022-02-19 16:00:45 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,106 @@
-let burger = document.getElementsByClassName('header__burger-btn')
-let menu = document.getElementsByClassName('header__nav-wrap')
-let overlay = document.getElementsByClassName('header__overlay')
+let randomNumber = Math.floor(Math.random() * 100) + 1;
 
+let guessSubmit = document.querySelector('.form__btn');
+let guessField = document.querySelector('.form__number');
 
+let resultParagraphs = document.querySelector('.result');
+let guesses = document.querySelector('.result__guesses');
+let counter = document.querySelector('.result__counter');
+let lowOrHi = document.querySelector('.result__low-or-high');
+let resetButton = document.querySelector('.reset');
 
-function toggleMenu(){
-    burger[0].classList.toggle('header__burger-btn--close')
-    menu[0].classList.toggle('header__nav-wrap--close')
-    overlay[0].classList.toggle('header__overlay--open')
+let guessesCount = 10;
+
+guessField.focus();
+
+guessSubmit.addEventListener('click', showResult);
+guessField.addEventListener('keydown', key => {
+    if (key.keyCode == '13') { //Добавили отслеживание нажатия enter 
+        showResult();
+    }
+});
+
+resetButton.addEventListener('click', resetGame);
+
+function showResult() {
+    checkGuess();
+    addPrevGuess();
+    guessesCount--;
+    showBalanceOfGuesses();
+
+    if (guessesCount == 0) {
+        lowOrHi.textContent = 'Game over!';
+        lowOrHi.style.background = '#cf131c';
+        addResetButton();
+        disableInput();
+    }
+
+    if (+(guessField.value) == randomNumber) {
+        lowOrHi.textContent = 'Правильно!';
+        lowOrHi.style.background = '#02780b';
+        addResetButton();
+        disableInput();
+    }
+
+    guessField.value = '';
+
 }
 
-burger[0].addEventListener("click", toggleMenu)
+function resetGame() {
+    randomNumber = Math.floor(Math.random() * 100) + 1;
 
+    guessSubmit.disabled = false;
+    guessField.disabled = false;
+
+    let guessesSpans = document.querySelectorAll('.result__guesses span');
+    for (let i = 0; i < guessesSpans.length; i++) {
+        guesses.removeChild(guessesSpans[i]);
+    }
+    guessesCount = 10;
+    showBalanceOfGuesses();
+    
+    lowOrHi.textContent = '';
+    lowOrHi.style.background = 'transparent';
+
+    removeResetButton();
+
+    guessField.focus();
+
+}
+
+function checkGuess() {
+    if (+(guessField.value) > randomNumber) {
+        lowOrHi.textContent = 'Меньше!';
+        lowOrHi.style.background = '#227094';
+    } else if (+(guessField.value) < randomNumber) {
+        lowOrHi.textContent = 'Больше!';
+        lowOrHi.style.background = '#cf131c';
+    }
+}
+function addPrevGuess() {
+    let prevGuess = document.createElement('span');
+
+    prevGuess.append(`${+(guessField.value)} `);
+    guesses.append(prevGuess);
+}
+
+function showBalanceOfGuesses() {
+    let balance = document.querySelector('.result__counter > span');
+    balance.textContent = guessesCount ;
+    
+
+}
+
+function addResetButton() {
+    resetButton.classList.add('reset--able');
+}
+
+function removeResetButton() {
+    resetButton.classList.remove('reset--able');
+
+}
+
+function disableInput() {
+    guessSubmit.disabled = true;
+    guessField.disabled = true;
+}
